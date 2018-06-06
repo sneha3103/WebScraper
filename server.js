dfc1aefc3aa1f9b9c6ba7f509779f66e379441ed
Sneha Dama <snehadama@Snehas-Air.lan>
2018-06-06 18:30:14 -0500

    styled the front end page

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var expressHandlebars = require("express-handlebars");
+var path = require("path");
 
 //Axios - promise based library. Similar to jquery ajax method
 var axios = require("axios");
@@ -20,6 +21,8 @@ var app = express();
 //Sets up an Express Router
 var router = express.Router();
 
+require("./config/routes")(router);
+
 //Allows request to go through router middleware
 app.use(router);
 
@@ -29,7 +32,7 @@ app.use(router);
 app.use(logger("dev"));
 
 //Handlebars
-app.enging("handlebars", expressHandlebars({
+app.engine("handlebars", expressHandlebars({
     defaultLayout: "main"
 }));
 
@@ -38,9 +41,11 @@ app.set("view engine", "handlebars");
 // Body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
 // Serving the public folder as a static directory
-app.use(express.static("public"));
 
-var db = process.env.MONGODB_URI || "mongodb://localhost/"
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static("public"));
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/webscraper"
 // Connect to the Mongo Db
 mongoose.connect(db,function(error){
     if(error) {
@@ -56,35 +61,43 @@ mongoose.connect(db,function(error){
 app.get("/scrape", function(req, res) {
     // First, we grab the body of the html with request
     axios.get("https://medium.com/").then(function(response) {
+        // console.log(response);
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
+    //   console.log(response.data);
 
       // Now, we grab every h3 within an article tag, and do the following:
     $(".extremeHero-postContent h3").each(function(i, element) {
-        console.log(element);
-        // Save an empty articles object
-        var articles = {};
+        // console.log("hello");
+        // console.log(element);
 
-        var articleTitle = element.children[0].data;
-        console.log(articleTitle);
+
+    
+        // Save an empty articles object
+        // var articles = {};
+
+        // var articleTitle = element.children[0].data;
+        // console.log(articleTitle);
   
-        var articleLink = element.parent.attribs.href;
-        console.log(articleLink);
+        // var articleLink = element.parent.attribs.href;
+        // console.log(articleLink);
   
-        articles.push(`<a href= "${articleLink}">${articleTitle}</a>`);
+        // articles.push(`<a href= "${articleLink}">${articleTitle}</a>`);
         
         //Creating a new article using the articleTitle.
-        db.Article.create(articleTitle)
-           .then(function(dbArticle){
-               console.log(dbArticle);
-           })
-           .catch(function(err){
-               return res.json(err);
-           });
+    //     db.Article.create(articleTitle)
+    //        .then(function(dbArticle){
+    //            console.log(dbArticle);
+    //        })
+    //        .catch(function(err){
+    //            return res.json(err);
+    //        });
+
+    // res.send(articles.join("<br>"));
 
     });
 
-    res.send(articles.join("<br>"));
+    
 
   });
 
